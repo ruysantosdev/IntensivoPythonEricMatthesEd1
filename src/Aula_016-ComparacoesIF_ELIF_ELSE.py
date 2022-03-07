3a4800af9887d8c7d436939a8c614734d67d6303
Ruy Santos <ruy.santos@gmail.com>
2022-03-07 10:08:16 -0300

    Alterand Aula_016-ComparacoesIF_ELIF_ELSE.py fazendo testes de IF em cascata ELIF.

--- src/Aula_016-ComparacoesIF_ELIF_ELSE.py
+++ src/Aula_016-ComparacoesIF_ELIF_ELSE.py
@@ -1,1 +1,24 @@
-#fonte:
+#fonte:https://www.youtube.com/watch?v=8yYTNVszyks&list=PLiu4wMrYDH6IvwMPVABUF4W0MFESxw0S_&index=16
+
+#IF THEM ELSE - ELIF NO PYTHON
+
+#Variável idade que armazena a idade do cliente (que irá comprar o ingresso)
+
+print("Informe a idade do cliente : ", end=' ')
+idade = int(input())
+
+#De acordo com a idade , vamos definir a variável preço de acordo com o
+#valor a ser pago
+if idade <= 5:
+    preco = 0
+elif idade <= 13:
+    preco = 5
+elif idade <= 18:
+    preco = 10
+elif idade <= 65:
+    preco = 15
+else:
+    preco = 0
+
+print(f"O preço do seu ingresso, para sua faixa de idade será de :  R$ {preco},00 reais, bom filme !!!")
+
